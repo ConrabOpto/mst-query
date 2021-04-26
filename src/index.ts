255d283ae7b01be38f6d662559b7e47fafc76869
Kim Ode <kim.gronqvist@conrab.se>
2021-04-26 16:39:54 +0200

    Test build thing

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import queryCache from './cache';
 
+export type { QueryFnType } from './QueryModelBase';
 export { queryCache };
 export { config, configure } from './config';
 export { useQuery, useLazyQuery, useMutation, useSubscription } from './hooks';
 export { create, createQuery, createMutation, createSubscription } from './create';
 export { MstQueryRef } from './MstQueryRef';
-export type { QueryFnType } from './QueryModelBase';
