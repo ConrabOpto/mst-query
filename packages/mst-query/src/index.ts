bf54416cd3bf99867987e35e38a34ec615a2f8e6
Kim Ode <kim.gronqvist@conrab.se>
2024-10-21 19:33:21 +0200

    Add missing exports

--- packages/mst-query/src/index.ts
+++ packages/mst-query/src/index.ts
@@ -1,6 +1,6 @@
 export { onMutate } from './MstQueryHandler';
-export { useQuery, useMutation, useVolatileQuery } from './hooks';
-export { createQuery, createMutation } from './create';
+export { useQuery, useMutation, useInfiniteQuery, useVolatileQuery } from './hooks';
+export { createQuery, createMutation, createInfiniteQuery } from './create';
 export { createRootStore, createModelStore } from './stores';
 export { QueryClient } from './QueryClient';
 export type { EndpointType } from './QueryClient';
