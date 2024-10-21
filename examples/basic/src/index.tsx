f8442a6a8100c96cf744075e99e66990fc8f9dd6
Kim Ode <kim.gronqvist@conrab.se>
2024-10-21 16:24:59 +0200

    Update basic example

--- examples/basic/src/index.tsx
+++ examples/basic/src/index.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { types, Instance } from 'mobx-state-tree';
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { types } from 'mobx-state-tree';
 import { observer } from 'mobx-react';
 import {
     createContext,
@@ -9,43 +10,34 @@ import {
     createRootStore,
     createModelStore,
 } from 'mst-query';
-import axios from 'axios';
 
-export const PostModel = types
-    .model('PostModel', {
-        id: types.identifierNumber,
-        title: types.string,
-        body: types.maybe(types.string),
-        userId: types.number,
-    })
-    .views((self) => ({
-        get hasContent() {
-            return !!self.body;
-        },
-    }));
-
-export interface PostModelType extends Instance<typeof PostModel> {}
-
-const getPosts = async () => {
-    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts');
-    return data.map((d: any) => ({ userId: d.userId, id: d.id, title: d.title }));
-};
+export const PostModel = types.model('PostModel', {
+    id: types.identifierNumber,
+    title: types.string,
+    body: types.maybe(types.string),
+    userId: types.number,
+});
 
 export const PostsQuery = createQuery('PostsQuery', {
     data: types.array(types.reference(PostModel)),
-    endpoint: getPosts,
+    async endpoint() {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const result = await response.json();
+        return result.map((d: any) => ({
+            userId: d.userId,
+            id: d.id,
+            title: d.title,
+        }));
+    },
 });
 
-const getPostById = async ({ request }: any) => {
-    const { id } = request;
-    const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
-    return data;
-};
-
 const PostQuery = createQuery('PostQuery', {
-    data: types.reference(PostModel),
-    request: types.model({ id: types.number }),
-    endpoint: getPostById,
+    data: types.reference(PostModel),    request: types.model({ id: types.number }),
+    async endpoint({ request }) {
+        const { id } = request;
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        return await response.json();
+    },
 });
 
 const PostStore = createModelStore('PostStore', PostModel)
@@ -66,17 +58,12 @@ const RootStore = createRootStore({
 const queryClient = new QueryClient({ RootStore });
 const { useRootStore, QueryClientProvider } = createContext(queryClient);
 
-type PostsProps = {
-    onSelectedPost: (post: PostModelType | null) => void;
-};
-
-const Posts = observer((props: PostsProps) => {
-    const { onSelectedPost } = props;
+const Posts: React.FC<any> = observer((props) => {
+    const { setSelectedPost } = props;
     const { postStore } = useRootStore();
-    const { data, error, isLoading } = useQuery(postStore.postsQuery, {
-        staleTime: 100000,
-        enabled: true,
-    });
+
+    const { data, error, isLoading } = useQuery(postStore.postsQuery);
+
     return (
         <div>
             <h1>Posts</h1>
@@ -91,10 +78,16 @@ const Posts = observer((props: PostsProps) => {
                             {data.map((post) => (
                                 <p key={post.id}>
                                     <a
-                                        onClick={() => onSelectedPost(post)}
+                                        onClick={() => {
+                                            setSelectedPost(post);
+                                        }}
                                         href="#"
                                         style={
-                                            post.hasContent
+                                            // We can access the query data here to show bold links for
+                                            // ones that are cached
+                                            postStore.postList.find(
+                                                (p) => p.id === post.id && p.body
+                                            )
                                                 ? {
                                                       fontWeight: 'bold',
                                                       color: 'green',
@@ -114,23 +107,19 @@ const Posts = observer((props: PostsProps) => {
     );
 });
 
-type PostProps = {
-    post: PostModelType;
-    onSelectedPost: (post: PostModelType | null) => void;
-};
-
-const Post = observer((props: PostProps) => {
-    const { post, onSelectedPost } = props;
+const Post: React.FC<any> = observer((props) => {
+    const { post, setSelectedPost } = props;
     const { postStore } = useRootStore();
+
     const { data, isLoading, error } = useQuery(postStore.postQuery, {
-        initialData: post,
-        request: { id: post.id }
+        request: { id: post.id },
+        enabled: !!post
     });
-    console.log(data?.body);
+
     return (
         <div>
             <div>
-                <a onClick={() => onSelectedPost(null)} href="#">
+                <a onClick={() => setSelectedPost(null)} href="#">
                     Back
                 </a>
             </div>
@@ -154,28 +143,24 @@ const Post = observer((props: PostProps) => {
 const env = {};
 
 const App = observer(() => {
-    const [selectedPost, setSelectedPost] = React.useState<PostModelType | null>(null);
+    const [selectedPost, setSelectedPost] = React.useState(null);
     return (
         <QueryClientProvider env={env}>
             <div>
-                <p>
-                    As you visit the posts below, you will notice them in a loading state the first
-                    time you load them. However, after you return to this list and click on any
-                    posts you have already visited again, you will see them load instantly and
-                    background refresh right before your eyes!{' '}
-                    <strong>
-                        (You may need to throttle your network speed to simulate longer loading
-                        sequences)
-                    </strong>
-                </p>
                 {selectedPost ? (
-                    <Post post={selectedPost} onSelectedPost={(post) => setSelectedPost(post)} />
+                    <Post post={selectedPost} setSelectedPost={setSelectedPost} />
                 ) : (
-                    <Posts onSelectedPost={(post) => setSelectedPost(post)} />
+                    <Posts setSelectedPost={setSelectedPost} />
                 )}
             </div>
         </QueryClientProvider>
     );
 });
 
-export default App;
+const rootElement: any = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
