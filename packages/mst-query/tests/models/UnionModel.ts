33ed064e335eddfa2bc1290480ef6ef2278f2c9a
Kim Ode <kimgronqvist@gmail.com>
2025-05-05 15:00:05 +0200

    Update tests to work with types in union (#90)
    * Update tests to work with types in union
    
    * fix whitespace

---
+++ packages/mst-query/tests/models/UnionModel.ts
@@ -0,0 +1,15 @@
+import { types } from 'mobx-state-tree';
+
+export const FixedModel = types.model('FixedModel', {
+    id: types.identifier,
+    kind: types.literal('FIXED'),
+    fixedValue: types.string,
+});
+
+export const FormatModel = types.model('FormatModel', {
+    id: types.identifier,
+    kind: types.literal('FORMAT'),
+    formatValue: types.string,
+});
+
+export const UnionModel = types.union(FixedModel, FormatModel);
